3b95d5c343b035ba2cd16840bff19c03ec5ab03a
Wt colab <93501803+hungwt@users.noreply.github.com>
2022-01-02 13:17:12 +0700

    Update RUNLXDE.sh

--- RUNLXDE.sh
+++ RUNLXDE.sh
@@ -1,1 +1,7 @@
-
+clear
+echo ENTER ĐỂ TIẾP TỤC
+read -p "==>: " ADAIWDHOIAWHD
+echo |=============================================|
+echo |                 RUNING...                   |
+echo |=============================================|
+docker run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
