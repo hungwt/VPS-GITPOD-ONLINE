c3e5338865d79a51f22ee280c6988a71b531cffd
Wt colab <93501803+hungwt@users.noreply.github.com>
2022-01-05 14:25:44 +0700

    Delete RUNLXDE.sh

--- RUNLXDE.sh
+++
@@ -1,7 +0,0 @@
-clear
-echo ENTER ĐỂ TIẾP TỤC
-read -p "==>: " ADAIWDHOIAWHD
-echo |=============================================|
-echo |                 RUNING...                   |
-echo |=============================================|
-docker run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
